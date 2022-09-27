28b28eb3b2277eaa39afd47bc8d2e98cebb495f3
Razorfox <113666175+F4bi0-R4z0rF0x@users.noreply.github.com>
2022-09-27 06:45:11 +0000

    si

--- main.cpp
+++ main.cpp
@@ -2,5 +2,14 @@
 
 int main() {
   std::cout << "Hello Fabionsky\n";
-}
 
+    int (x);
+    int (y);
+    x=10;
+    y=20;
+    int z=x+y;
+std::cout << "Il tuo risultato è:\n";
+  std::cout << z;
+  std::cout << 
+    " Spero di esserti stato udile\n";
+}
